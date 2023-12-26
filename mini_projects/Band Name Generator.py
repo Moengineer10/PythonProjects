1c97d8a3d4b7161b4a4dfd2093068d3a1743cb8e
Moengineer10 <126132534+Moengineer10@users.noreply.github.com>
2023-12-26 08:26:59 -0600

    Add files via upload

---
+++ mini_projects/Band Name Generator.py
@@ -0,0 +1,14 @@
+### Generating a name creator for a music band, using city and pet names
+
+### Introductory sentence to user
+print("Welcome to the band Name Generator.") 
+
+### Collection of first name, user will enter city name in a assigned variables used later on for string addition
+a = (input("What's the name of the city you grew up in?"))
+print(a)
+### Collection of first name, user will enter petname name in a assigned variables used later on for string addition
+b= (input("What's your pet's name?"))
+print(b)
+
+### string(s) combination
+print('Your band name could be', a, b)
