30af76ddd07ab917e09b97211adbd708a8959643
Moengineer10 <126132534+Moengineer10@users.noreply.github.com>
2023-12-26 08:28:28 -0600

    Add files via upload

---
+++ mini_projects/Odd or Even.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 21 08:07:05 2023
+
+@author: moham
+"""
+
+number =int(input("Enter your number:"))
+
+if number % 2 == 0:
+    print("This is an even number!")
+else:
+    print("This is an odd number is odd!")
